e68ac3c6640820fd001f4aa3396ea8e1cab06c75
retotito <retotito@gmail.com>
2017-09-18 16:43:02 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  post = {
+    title: "Title",
+    isFavorite: true
+  }
+
+  courses = [
+    {id:1, name: "course1"},
+    {id:2, name: "course2"},
+    {id:3, name: "course3"},
+    {id:4, name: "course4"},
+    {id:5, name: "course5"}
+  ]
+
+  myElement = {
+    name: "Max Muster"
+  };
+
+  onLoadList () {
+    this.courses = [
+      {id:1, name: "course1"},
+      {id:2, name: "course2"},
+      {id:3, name: "course3"},
+      {id:4, name: "course4"},
+      {id:5, name: "course5"},
+      {id:6, name: "course6"}
+    ]
+  }
+
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
+  }
+  
+
+  onFavoriteChanged($event) {
+    console.log("changed", $event);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
+import { PostService } from './services/post/post.service';
+import { HttpModule } from '@angular/http';
+import { CoursesService } from './courses.service';
+import { AuthorsService } from './authors/authors.service';
+import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorComponent } from './author/author.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
+import { InputFormatDirective } from './input-format.directive';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent,
+    AuthorComponent,
+    FavoriteComponent,
+    TitleCasePipe,
+    InputFormatDirective,
+    ContactFormComponent,
+    SignupFormComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
   ],
-  providers: [],
+  providers: [
+    PostService,
+    CoursesService,
+    AuthorsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
